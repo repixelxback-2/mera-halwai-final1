abd056cff31e340e7960327f8a5b8a2868596002
virtualheet <heetvavadiya456@gmail.com>
2025-08-13 23:51:46 +0530

    mobilr-8

--- src/components/Home5.jsx
+++ src/components/Home5.jsx
@@ -9,9 +9,9 @@ const Home5 = () => {
                 padding: "clamp(1rem,5vw,200rem)"
             }}
         >
-            <div className="flex flex-col  items-center w-[90%] gap-10 md:gap-0 h-full">
+            <div className="flex flex-col  items-center w-[90%] gap-10 md:gap-10 h-full">
 
-                <div className="text-[#FF0000] text-center md:text-left text-xl inter font-medium w-full translate-y-5">
+                <div className="text-[#FF0000] text-center md:text-left text-xl inter font-medium w-full translate-y-6 ">
                     How it works
                 </div>
                 <div className="text-2xl w-full sm:text-5xl leading-snug md:text-5xl pixel text-center md:text-left text-[#8A3E1D]"
@@ -20,7 +20,7 @@ const Home5 = () => {
                 </div>
 
 
-                <div className="w-full justify-center grid grid-rows-3 md:grid-cols-3 items-center gap-5 relative h-full md:h-[90%]">
+                <div className="w-full justify-center flex flex-col md:flex-row items-center gap-5 relative h-full md:h-[90%]">
                     <div className="flex absolute w-full z-10 justify-center  h-[100%]">
 
                         <Image
@@ -28,7 +28,7 @@ const Home5 = () => {
                             alt="home3"
                             width={1000}
                             height={1000}
-                            className=' w-[100%] rotate-90 md:w-[70%]  h-full'
+                            className=' w-[100%] rotate-90 md:w-[70%] md:rotate-0 md:h-[40vh] h-full'
                         />
                     </div>
                     <Image
@@ -36,21 +36,21 @@ const Home5 = () => {
                         alt="home3"
                         width={1000}
                         height={1000}
-                        className='w-full relative z-20 bg-gradient-to-l from-[#F1EDE2] to-[#fffefc] rounded-4xl md:h-[40%]'
+                        className='w-full md:w-1/3 relative z-20 bg-gradient-to-l from-[#F1EDE2] to-[#fffefc] rounded-4xl md:h-fit'
                     />
                     <Image
                         src="/a2.svg"
                         alt="home3"
                         width={1000}
                         height={1000}
-                        className='w-full relative z-20 bg-gradient-to-l from-[#F1EDE2] to-[#fffefc] rounded-4xl md:h-[40%]'
+                        className='w-full md:w-1/3 relative z-20 bg-gradient-to-l from-[#F1EDE2] to-[#fffefc] rounded-4xl md:h-fit'
                     />
                     <Image
                         src="/a3.svg"
                         alt="home3"
                         width={1000}
                         height={1000}
-                        className='w-full relative z-20 bg-gradient-to-l from-[#F1EDE2] to-[#fffefc] rounded-4xl md:h-[40%]'
+                        className='w-full md:w-1/3 relative z-20 bg-gradient-to-l from-[#F1EDE2] to-[#fffefc] rounded-4xl md:h-fit'
                     />
                 </div>
 
