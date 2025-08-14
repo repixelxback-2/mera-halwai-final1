981ed6429aeb291a4a81fecc53b01fcc13a973db
virtualheet <heetvavadiya456@gmail.com>
2025-08-14 23:53:04 +0530

    doo

--- src/components/Home7.jsx
+++ src/components/Home7.jsx
@@ -4,12 +4,12 @@ import Button from './Button'
 
 const Home7 = () => {
     return (
-        <div className='w-full h-fit flex justify-center'
+        <div className='w-full h-full flex justify-center'
             style={{
                 padding: "clamp(1rem,5vw,200rem)"
             }}
         >
-            <div className="flex flex-col gap-10 items-center w-[90%] h-full">
+            <div className="flex flex-col gap-10 items-center w-[100%] h-full">
                 <div className="flex flex-col gap-2 w-full">
                     <div className="text-[#FF0000] text-center md:text-right text-xl inter font-medium w-full">
                         Testimonials
@@ -21,21 +21,21 @@ const Home7 = () => {
                 </div>
 
 
-                <div className="w-full justify-center grid grid-row-3 md:grid-cols-3 items-center gap-5 relative h-[60%]">
+                <div className="w-full justify-center grid grid-row-3 md:grid-cols-3 items-center gap-5 relative h-full ">
 
                     <Image
                         src="/c1.svg"
                         alt="home3"
                         width={400}
                         height={400}
-                        className='w-full relative z-20 shadow-2xl   shadow-black/20  rounded-4xl h-fit'
+                        className='w-96 md:w-full  relative z-20 shadow-2xl   shadow-black/20  rounded-4xl h-full'
                     />
                     <Image
                         src="/c2.svg"
                         alt="hme3"
                         width={400}
                         height={400}
-                        className='w-full relative z-20  shadow-2xl shadow-black/20  rounded-4xl h-fit'
+                        className='w-full relative z-20  shadow-2xl shadow-black/20  rounded-4xl h-full'
                     />
                     <Image
                         src="/c3.svg"
