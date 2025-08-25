5c6305930bedbd2ed578aa3148c03c7beff0bbdb
virtualheet <heetvavadiya456@gmail.com>
2025-08-26 01:10:11 +0530

    woop woop

--- src/components/Home7.jsx
+++ src/components/Home7.jsx
@@ -11,24 +11,24 @@ const Home7 = () => {
         >
             <div className="flex flex-col gap-10 items-center w-[100%] h-full">
                 <div className="flex flex-col gap-2 w-full">
-                    <div className="text-[#FF0000] text-center md:text-right text-xl inter font-medium w-full">
+                    <div className="text-[#FF0000] text-center xl:text-right text-xl inter font-medium w-full">
                         Testimonials
                     </div>
-                    <div className="text-xl w-full sm:text-5xl leading-snug md:text-5xl pixel text-center md:text-right text-[#8A3E1D]"
+                    <div className="text-xl w-full sm:text-5xl leading-snug xl:text-5xl pixel text-center xl:text-right text-[#8A3E1D]"
                     >
                         Sweet words from happy customers
                     </div>
                 </div>
 
 
-                <div className="w-full justify-center grid grid-row-3 md:grid-cols-3 items-center gap-5 relative h-full ">
+                <div className="w-full justify-center grid grid-row-3 xl:grid-cols-3 items-center gap-5 relative h-full ">
 
                     <Image
                         src="/c1.png"
                         alt="home3"
                         width={400}
                         height={400}
-                        className='w-96 md:w-full  relative z-20 shadow-2xl   shadow-black/20  rounded-4xl h-full'
+                        className='w-full xl:w-full  relative z-20 shadow-2xl   shadow-black/20  rounded-4xl h-full'
                     />
                     <Image
                         src="/c2.png"
